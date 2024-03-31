ebed33134ed6205e9dd5fadb212cbc567fa2c96c
Jisun696 <istiakkashem35@gmail.com>
2024-03-31 06:01:45 +0600

    added card-ui for the each dynamic data(blog) and displayed them

---
+++ src/components/blog-card/BlogCard.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { Link } from 'react-router-dom';
+import placeholderImage from '../../assets/images/404.jpg';
+
+const BlogCard = ({ blog }) => {
+  const { published_at, cover_image, title, description, id } = blog;
+  return (
+    <div>
+      <Link to={`/blogs/${id}`} rel="noopener noreferrer" className="max-w-sm mx-auto group hover:no-underline focus:no-underline bg-gray-900 dark:bg-gray-50">
+        <img role="presentation" className="object-cover w-full rounded h-44 bg-gray-500 dark:bg-gray-500" src={cover_image || placeholderImage} />
+        <div className="p-6 space-y-2">
+          <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">{title}</h3>
+          <span className="text-xs text-gray-400 dark:text-gray-600">{new Date(published_at).toLocaleDateString()}</span>
+          <p>{description}</p>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default BlogCard;
